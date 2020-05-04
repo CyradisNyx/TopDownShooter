4b205ae98ba5f914bc1ecd7a560d8a6f7acc654b
AllSeeingGoggles <64765275+AllSeeingGoggles@users.noreply.github.com>
2020-05-04 17:17:21 +1200

    Added DUMMYTEXT
    I added a """"script"""" in the resources folder. Please ignore :)

---
+++ Assets/Resources/DUMMYTEXT.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DUMMYTEXT : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("ahahahha");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
