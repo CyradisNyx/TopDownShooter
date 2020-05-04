8d25498aad575f56b7d1a8c30778937850774e56
Sarah <sarahbeavan@gmail.com>
2020-05-04 19:20:52 +1200

    Camera Follow Player

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public GameObject target;
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
+
+    private Vector3 velocity = Vector3.zero;
+    private float smoothing = 0.3f;
+
+    void FixedUpdate()
+    {
+        Vector3 targetPos = target.transform.position;
+        targetPos.x += xOffset;
+        targetPos.y += yOffset;
+        targetPos.z += zOffset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothing);
+    }
+}
