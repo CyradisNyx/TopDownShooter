e4c56906bc0f90d7b75d9e3682adce4d124c3787
Sarah <sarahbeavan@gmail.com>
2020-05-04 22:30:56 +1200

    Door/Bars lower/raise
    Implemented Pressable interface for further use. Bars that raise/lower

--- Assets/Scripts/ButtonTouch.cs
+++ Assets/Scripts/ButtonTouch.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class ButtonTouch : MonoBehaviour
 {
+    public List<GameObject> pressMe; // Who do I affect
+    public List<GameObject> canPress; // Who can press me
+
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("ouch");
-        EventMaster.Instance.ButtonPress(collision.gameObject);
+        for (int i = 0; i < canPress.Count; i++)
+        {
+            if (canPress[i] == collision.gameObject)
+            {
+                for (int j = 0; j < pressMe.Count; j++)
+                {
+                    pressMe[j].GetComponent<IPressable>().Press();
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IPressable
+{
+    public float movementSpeed = 3f;
+    protected bool locked = false;
+    protected bool raised = true;
+
+    public void Press()
+    {
+        if (!locked)
+        {
+            locked = true;
+            StartCoroutine(Move(raised));
+        }
+    }
+
+    IEnumerator Move(bool lower)
+    {
+        Vector3 goTo;
+        float elapsedTime = 0;
+
+        if (lower) { goTo = new Vector3(transform.position.x, transform.position.y - 3, transform.position.z); }
+        else { goTo = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z); }
+        
+        while (elapsedTime < movementSpeed)
+        {
+            transform.position = Vector3.Lerp(transform.position, goTo, (elapsedTime / this.movementSpeed));
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.position = goTo;
+
+        this.locked = false;
+        this.raised = !this.raised;
+
+        yield break;
+    }
+}
