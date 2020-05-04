5b41136813ff9d7d986adf9ad38ee7cd5fe546c1
Sarah <sarahbeavan@gmail.com>
2020-05-04 18:47:31 +1200

    Player Movement
    He's alive!

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+
+    Rigidbody rb;
+    Vector3 movement;
+
+    void Start()
+    {
+        this.rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        this.movement.x = Input.GetAxisRaw("Horizontal");
+        this.movement.y = 0f;
+        this.movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //Move
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
