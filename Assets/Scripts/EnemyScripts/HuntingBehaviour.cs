ccd495f6eaebd9edd6f3937ddfed4d894c415c00
Sarah <sarahbeavan@gmail.com>
2020-05-05 15:08:23 +1200

    Starting Hunting Behaviour
    Not Functional

--- Assets/Scripts/EnemyScripts/HuntingBehaviour.cs
+++ Assets/Scripts/EnemyScripts/HuntingBehaviour.cs
@@ -5,35 +5,50 @@ using UnityEngine;
 public class HuntingBehaviour : StateMachineBehaviour
 {
     public UnityEngine.AI.NavMeshAgent agent;
-    public float viewDistance;
+    public GameObject playerTarget;
     public float walkSpeed;
+    public float attackRangeStart;
+    public float attackRangeEnd;
+
+    protected bool lockOn;
+    protected Vector3 lastSeen;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         this.agent = animator.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
         agent.speed = walkSpeed;
+
+        this.playerTarget = GameObject.Find("Player");
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        RaycastHit hitInfo;
+        if (Physics.Raycast(animator.transform.position, animator.transform.forward, out hitInfo, attackRangeEnd))
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Player"))
+            {
+                lockOn = true;
+                lastSeen = playerTarget.transform.position;
+                Debug.DrawLine(animator.transform.position, hitInfo.point, Color.red);
+            }
+            else
+            {
+                lockOn = false;
+                Debug.DrawLine(animator.transform.position, hitInfo.point, Color.white);
+            }
+        }
+        else
+        {
+            Debug.DrawLine(animator.transform.position, animator.transform.position + animator.transform.forward * attackRangeEnd, Color.green);
+        }
+
+        if (lockOn) { agent.destination = playerTarget.transform.position; }
+        else
+        {
+            Debug.Log("going to last known position");
+            agent.destination = lastSeen;
+        }
+
     }
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
