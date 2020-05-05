aa04e509a66ee66255ea135a0f6b9d4881e0eaca
Sarah <sarahbeavan@gmail.com>
2020-05-05 22:06:52 +1200

    UI clickedy click
    Left/Right click for guns are assigned

---
+++ Assets/Scripts/Guns/BulletCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCounter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.parent.name == "LeftGun")
+        {
+            EventMaster.Instance.onLeftClick += OnClick;
+        }
+        else if (transform.parent.name == "RightGun")
+        {
+            EventMaster.Instance.onRightClick += OnClick;
+        }
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("click");
+    }
+}
