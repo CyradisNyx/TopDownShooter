9a5de81334c5b0a7c9919ce21d63181f55090b11
Sarah <sarahbeavan@gmail.com>
2020-05-09 12:58:49 +1200

    GunPickup Collision detection

--- Assets/Scripts/Guns/GunPickup.cs
+++ Assets/Scripts/Guns/GunPickup.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunPickup : MonoBehaviour
+public class Pickup : MonoBehaviour
 {
     public float turnDegrees;
 
     void FixedUpdate()
     {
         gameObject.transform.Rotate(0, turnDegrees * Time.deltaTime, 0, Space.World);
-    }
-
-    void OnCollisionEnter()
-    {
-        Destroy(this.gameObject);
     }
 }
