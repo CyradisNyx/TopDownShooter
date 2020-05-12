299c15d1dec33b91a3fb8a2ae485e7af6349ca55
Sarah <sarahbeavan@gmail.com>
2020-05-12 19:45:04 +1200

    Camera Bound at end
    Prepping for load next scene

---
+++ Assets/CameraStop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStop : MonoBehaviour
+{
+    Camera cam;
+    GameObject player;
+
+    void Start()
+    {
+        cam = Camera.main;
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag != "Player") { return; }
+
+        cam.gameObject.GetComponent<FollowCam>().enabled = false;
+    }
+}
---
+++ Assets/LoadNext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNext : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag != "Player") { return; }
+
+        Debug.Log("Load Next Scene");
+    }
+}
