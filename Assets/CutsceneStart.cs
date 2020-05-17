8468ff4d804fbae24ad40cfc96c1cc7045c3da1c
Sarah <sarahbeavan@gmail.com>
2020-05-18 09:21:12 +1200

    More setup

---
+++ Assets/CutsceneStart.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneStart : MonoBehaviour
+{
+    Camera cam;
+    GameObject player;
+
+    public enum Types
+    {
+        TUTORIAL,
+    }
+
+    public Types _type;
+
+    void Start()
+    {
+        cam = Camera.main;
+        player = GameObject.FindWithTag("Player");
+        EventMaster.Instance.onCutsceneEnd += CutsceneEnd;
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag != "Player") { return; }
+
+        cam.gameObject.GetComponent<FollowCam>().enabled = false;
+        player.GetComponent<PlayerMovement>().enabled = false;
+
+        EventMaster.Instance.CutsceneStart(_type.ToString());
+    }
+
+    public void CutsceneEnd(string type)
+    {
+        if (type != _type.ToString()) { return; }
+
+        cam.gameObject.GetComponent<FollowCam>().enabled = true;
+        player.GetComponent<PlayerMovement>().enabled = true;
+
+        Destroy(this.gameObject);
+    }
+}
