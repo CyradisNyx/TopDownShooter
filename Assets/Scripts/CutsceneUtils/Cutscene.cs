9d6e489fd6b9f26d15076f0ceabba9a34c72bb9d
Sarah <sarahbeavan@gmail.com>
2020-05-28 13:07:36 +1200

    Setup parent class for cutscenes

---
+++ Assets/Scripts/CutsceneUtils/Cutscene.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+    Camera cam;
+    GameObject player;
+
+    public Transform scenePos;
+    public List<GameObject> actors;
+    public GameObject textBox;
+
+    public virtual string cutsceneType { get; set; }
+    bool active = false;
+    Vector3 scenePosActual;
+
+    void Start()
+    {
+        cam = Camera.main;
+        player = GameObject.FindWithTag("Player");
+        this.scenePosActual = scenePos.position;
+        this.scenePosActual.y = cam.gameObject.transform.position.y;
+
+        EventMaster.Instance.onCutsceneStart += CutsceneStart;
+    }
+
+    void Update()
+    {
+        if (!active) { return; }
+        cam.gameObject.transform.position = Vector3.Lerp(cam.gameObject.transform.position, scenePosActual, Time.deltaTime);
+    }
+
+    public void CutsceneStart(string type)
+    {
+        if (type != cutsceneType) { return; }
+
+        this.active = true;
+        textBox.SetActive(true);
+
+        StartCoroutine(WaitSeconds(5f));
+
+        // Add text bubbles and play noise over actors
+    }
+
+    IEnumerator WaitSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        this.CloseScene();
+    }
+
+    void CloseScene()
+    {
+        this.active = false;
+        textBox.SetActive(false);
+        EventMaster.Instance.CutsceneEnd(cutsceneType);
+    }
+}
--- Assets/Scripts/CutsceneUtils/TutorialEndCutscene.cs
+++ Assets/Scripts/CutsceneUtils/TutorialEndCutscene.cs
@@ -2,58 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialEndCutscene : MonoBehaviour
+public class TutorialEndCutscene : Cutscene
 {
-    Camera cam;
-    GameObject player;
-
-    public Transform scenePos;
-    public List<GameObject> actors;
-    public GameObject textBox;
-
-    bool active = false;
-    Vector3 scenePosActual;
-
-    void Start()
-    {
-        cam = Camera.main;
-        player = GameObject.FindWithTag("Player");
-        this.scenePosActual = scenePos.position;
-        this.scenePosActual.y = cam.gameObject.transform.position.y;
-
-        EventMaster.Instance.onCutsceneStart += CutsceneStart;
-    }
-
-    void Update()
-    {
-        if (!active) { return; }
-        cam.gameObject.transform.position = Vector3.Lerp(cam.gameObject.transform.position, scenePosActual, Time.deltaTime);
-    }
-
-    public void CutsceneStart(string type)
-    {
-        if (type != "TUTORIALEND") { return; }
-
-        this.active = true;
-        textBox.SetActive(true);
-
-        StartCoroutine(WaitSeconds(5f));
-
-        // Add text bubbles and play noise over actors
-    }
-
-    IEnumerator WaitSeconds(float seconds)
-    {
-        yield return new WaitForSeconds(seconds);
-
-        this.CloseScene();
-
-    }
-
-    void CloseScene()
-    {
-        this.active = false;
-        textBox.SetActive(false);
-        EventMaster.Instance.CutsceneEnd("TUTORIAL");
-    }
+    public override string cutsceneType { get { return "TUTORIALEND"; } }
 }
