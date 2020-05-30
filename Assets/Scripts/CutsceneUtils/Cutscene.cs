85a115914017c36339dd8707d0c51b5fe18bb7c6
Sarah <sarahbeavan@gmail.com>
2020-05-31 00:18:22 +1200

    Movement tweaks
    Swapped back to MoveTowards, as cutscene was hanging at the end, when the actor was already offscreen

--- Assets/Scripts/CutsceneUtils/Cutscene.cs
+++ Assets/Scripts/CutsceneUtils/Cutscene.cs
@@ -93,12 +93,12 @@ public class Cutscene : MonoBehaviour
         yield return null;
     }
 
-    protected IEnumerator Move(int actorID, Vector3 moveTo, float speed = 0.5f)
+    protected IEnumerator Move(int actorID, Vector3 moveTo, float speed = 7f)
     {
         while (Vector3.Distance(actors[actorID].transform.position, moveTo) >= 0.5f)
         {
             Debug.Log("moving");
-            actors[actorID].transform.position = Vector3.Lerp(actors[actorID].transform.position, moveTo, speed * Time.deltaTime);
+            actors[actorID].transform.position = Vector3.MoveTowards(actors[actorID].transform.position, moveTo, speed * Time.deltaTime);
             yield return null;
         }
 
