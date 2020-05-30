89c9a605ce846f93b612163894ccb8dab5763b56
Sarah <sarahbeavan@gmail.com>
2020-05-31 01:16:12 +1200

    Start cutscene completed

--- Assets/Scripts/CutsceneUtils/TutorialStartCutscene.cs
+++ Assets/Scripts/CutsceneUtils/TutorialStartCutscene.cs
@@ -11,7 +11,12 @@ public class TutorialStartCutscene : Cutscene
     {
         Debug.Log("CutsceneStart");
 
-        yield return TypeWriter("you're still alive. good...", 0);
+        yield return TypeWriter("you're still alive...", 0);
+        yield return TypeWriter("that power short seems like it took out most of the building", 0);
+        yield return TypeWriter("we might be able to make a run for it", 0);
+        yield return TypeWriter("...", 0);
+        yield return TypeWriter("you don't have a weapon though", 0);
+        yield return TypeWriter("i'll scout ahead, so be careful", 0);
 
         yield return Move(0, positions[0].transform.position, 5f);
         yield return Move(0, positions[1].transform.position, 5f);
